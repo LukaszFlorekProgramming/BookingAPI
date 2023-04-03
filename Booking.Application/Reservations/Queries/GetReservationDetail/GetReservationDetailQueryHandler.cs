cd0033a4e1b477a9804ae00a064e42502d00537b
Lukasz Florek <lukaszflorek2@gmail.com>
2023-04-03 17:00:56 +0200

    add queries

---
+++ Booking.Application/Reservations/Queries/GetReservationDetail/GetReservationDetailQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using Booking.Application.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Application.Reservations.Queries.GetReservationDetail
+{
+    public class GetReservationDetailQueryHandler : IRequestHandler<GetReservationDetailQuery, ReservationDetailVm>
+    {
+        private readonly IReservationDbContext _context;
+        public GetReservationDetailQueryHandler(IReservationDbContext roomDbContext) 
+        { 
+            _context = roomDbContext;
+        }
+        public async Task<ReservationDetailVm> Handle(GetReservationDetailQuery request, CancellationToken cancellationToken)
+        {
+            //var reservation = await _context.Res
+            throw new NotImplementedException();
+        }
+    }
+}

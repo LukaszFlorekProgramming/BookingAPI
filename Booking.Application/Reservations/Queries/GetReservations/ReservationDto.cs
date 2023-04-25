a137699fb6e1c5ad23b8e77137e6abb12d20eab6
Lukasz Florek <lukaszflorek2@gmail.com>
2023-04-25 21:34:28 +0200

    adding a column idroom in Reservation table

--- Booking.Application/Reservations/Queries/GetReservations/ReservationDto.cs
+++ Booking.Application/Reservations/Queries/GetReservations/ReservationDto.cs
@@ -14,6 +14,7 @@ namespace Booking.Application.Reservations.Queries.GetReservations
         public int Id { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+        public int? RoomId { get; set; }
 
         public void Mapping(Profile profile)
         {

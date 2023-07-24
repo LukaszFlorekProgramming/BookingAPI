4fbfe0faeca4c066c02a10e3ea09defaaea7523e
Lukasz Florek <lukaszflorek2@gmail.com>
2023-07-24 21:04:42 +0200

    fixing search

--- Booking.Application/Rooms/Queries/GetRoomsAndImages/GetRoomsAndImagesQueryHandler.cs
+++ Booking.Application/Rooms/Queries/GetRoomsAndImages/GetRoomsAndImagesQueryHandler.cs
@@ -24,7 +24,10 @@ namespace Booking.Application.Rooms.Queries.GetRoomsAndImages
         }
         public async Task<RoomsAndImagesVm> Handle(GetRoomsAndImagesQuery request, CancellationToken cancellationToken)
         {
-            var RoomsAndImages = await _context.Rooms.Include(x => x.Hotel).ProjectTo<RoomAndImageDto>(_mapper.ConfigurationProvider).ToListAsync();
+            var RoomsAndImages = await _context.Rooms.Include(x => x.Hotel)
+                .ProjectTo<RoomAndImageDto>(_mapper.ConfigurationProvider)
+                .OrderByDescending(x => x.NameHotel)
+                .ToListAsync();
 
             return new RoomsAndImagesVm() { RoomsAndImages = RoomsAndImages };
         }
